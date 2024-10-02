37faa93543246311301241a3c044cb3344539753
Blckdev <23felipecamejo@gmail.com>
2024-10-01 21:53:06 -0300

    aparecen los productos

--- js/catalogo.js
+++ js/catalogo.js
@@ -8,7 +8,7 @@ document.querySelectorAll('.dropdown-item input[type="checkbox"]').forEach(funct
 
 let producto = document.getElementById("producto1");
 
-prod.addEventListener('click', function() {
+producto.addEventListener('click', function() {
     productoSeleccionado = producto.nombre;
     cargarProducto();
 });
@@ -16,77 +16,84 @@ prod.addEventListener('click', function() {
 function cargarCatalogo() {
     const prod = JSON.parse(localStorage.getItem('productos')) || [];
 
+
     if (prod.length === 0){ /* aunque parezca raro es con tres '=' */
         return;
     }
 
     const contenedorPadre = document.getElementById("prods");
 
-    document.getElementById("crearDiv").addEventListener("click", function() {
-        prod.forEach(element => {
-            const nuevoRectangulo = document.createElement("article");
 
-            const nuevaImagen = document.createElement("img");
+    prod.forEach(element => {
+        const nuevoRectangulo = document.createElement("article");
+        const nuevaImagen = document.createElement("img");
+        const nuevoDemas = document.createElement("div");
 
-            const padreEstrellasTitulo = document.createElement("div");
-            const nuevoTitulo = document.createElement("div");
-            const nuevoConjuntoEstrellas = document.createElement("div");
+        const padreItemEstrellas = document.createElement("div");
+        const nuevoTitulo = document.createElement("div");
+        const nuevoConjuntoEstrellas = document.createElement("div");
 
-            const nuevaTienda = document.createElement("div");
+        const nuevaTienda = document.createElement("div");
 
-            const padrePrecioCarrito = document.createElement("div");
-            const nuevoPrecio = document.createElement("div");
-            const nuevoCarrito = document.createElement("div");
+        const padrePrecioCarrito = document.createElement("div");
+        const nuevoPrecio = document.createElement("div");
+        const nuevoCarrito = document.createElement("div");
 
-            nuevaImagen.src = element.nuevaImagen; // inchequeable part 1: hay mas de una imagen(?
+        nuevaImagen.src = element.nuevaImagen; // inchequeable part 1: hay mas de una imagen(?
 
-            let estrellasMarcadas = element.estrellas;
+        let estrellasMarcadas = element.estrellas;
 
-            for (let i = 1; i <= 5; i++) {
-                let star = document.createElement("li");
-                
-                if (estrellasMarcadas > 0) {
-                    star.classList.add("fa", "fa-star", "checked");
-                    estrellasMarcadas--;
-                } else {
-                    star.classList.add("fa", "fa-star");
-                }
-                nuevoConjuntoEstrellas.appendChild(star);
+        for (let i = 1; i <= 5; i++) {
+            let star = document.createElement("span");
+            if (estrellasMarcadas > 0) {
+                star.classList.add("fa", "fa-star", "checked");
+                estrellasMarcadas--;
+            } else {
+                star.classList.add("fa", "fa-star");
             }
 
-            //agregar contenido al div
-            nuevoTitulo.innerHTML = element.nombre;
-            nuevoPrecio.innerHTML = element.precio;
-            nuevaTienda.innerHTML = element.tienda; // inchequeable part 2: no existe tal atributo
+            nuevoConjuntoEstrellas.appendChild(star);
+        }
 
-            //agregar clases o atributos si es necesario
-            nuevoRectangulo.classList.add("rectangle-1");
+        //agregar contenido al div
+        nuevoTitulo.innerHTML = element.nombre;
+        nuevoPrecio.innerHTML = element.precio;
+        nuevaTienda.innerHTML = element.tienda; // inchequeable part 2: no existe tal atributo
 
-            nuevaImagen.classList.add("image-1"); 
+        //agregar clases o atributos si es necesario
+        nuevoRectangulo.classList.add("rectangle-1", "row");
 
-            padreEstrellasTitulo.classList.add("row");
-            nuevoConjuntoEstrellas.classList.add("col-md", "col-sm-12", "conjunto_estrellas");
-            nuevoTitulo.classList.add("col-md-6", "col-sm-12", "item-1");
+        nuevaImagen.classList.add("col-3", "image-1");
+        
+        nuevoDemas.classList.add("col-9", "row", "todo-lodemas");
 
-            nuevaTienda.classList.add("tienda-x");
+        padreItemEstrellas.classList.add("row", "col-12", "item-estrellas");
+        nuevoTitulo.classList.add("col", "item-1");
+        nuevoConjuntoEstrellas.classList.add("col", "conjunto_estrellas");
+        
+        
+        nuevaTienda.classList.add("tienda-x", "col-12");
 
-            padrePrecioCarrito.classList.add("row", "precio-carrito");
-            nuevoPrecio.classList.add("col-6", "precio");
-            nuevoCarrito.classList.add("col", "carrito", "fa-solid", "fa-cart-shopping");
+        padrePrecioCarrito.classList.add("row", "precio-carrito");
+        nuevoPrecio.classList.add("col-6", "precio");
+        nuevoCarrito.classList.add("col", "carrito", "fa-solid", "fa-cart-shopping");
 
-            //meterlos contenedores en el contenedor principal
-            padreEstrellasTitulo.appendChild(nuevoTitulo);
-            padreEstrellasTitulo.appendChild(nuevoConjuntoEstrellas);
+        //meterlos contenedores en el contenedor principal
+        padreItemEstrellas.appendChild(nuevoTitulo);
+        padreItemEstrellas.appendChild(nuevoConjuntoEstrellas);
     
-            padrePrecioCarrito.appendChild(nuevoPrecio);
-            padrePrecioCarrito.appendChild(nuevoCarrito);
+        padrePrecioCarrito.appendChild(nuevoPrecio);
+        padrePrecioCarrito.appendChild(nuevoCarrito);
 
-            nuevoRectangulo.appendChild(nuevaImagen);
-            nuevoRectangulo.appendChild(padreEstrellasTitulo);
-            nuevoRectangulo.appendChild(nuevaTienda);
-            nuevoRectangulo.appendChild(padrePrecioCarrito);
+        nuevoDemas.appendChild(padreItemEstrellas);
+        nuevoDemas.appendChild(nuevaTienda);
+        nuevoDemas.appendChild(padrePrecioCarrito);
 
-            contenedorPadre.appendChild(nuevoRectangulo);
-        });
+        nuevoRectangulo.appendChild(nuevaImagen);
+        nuevoRectangulo.appendChild(nuevoDemas);
+
+        contenedorPadre.appendChild(nuevoRectangulo);
     });
 }
+
+cargarCatalogo();
